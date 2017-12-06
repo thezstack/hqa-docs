8ae03bccaf9a093bdee57859675582e8c4755c31
Allen Hai <hai@growinfinitely.com>
2017-12-06 16:51:31 -0600

    slider image set for full viewport with no whitespace around edges

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 // Helpers
 
-import Link from 'next/link'
+{
+  /*import Link from 'next/link'*/
+}
+import Slider from '../components/slider'
 
-{/* import redirect from '../lib/redirect'
+{
+  /* import redirect from '../lib/redirect'
 
-export default redirect('/docs') */}
+export default redirect('/docs') */
+}
 
-export default () => (
-  <div>
-    <h1>welcome to hqa</h1>
-    <Link href="/docs">
-      <a>info</a>
-    </Link>
-  </div>
-)
+export default function HQA() {
+  return (
+    <div>
+      <Slider />
+      <style jsx global>{`
+          * {
+            margin: 0px;
+            padding: 0px;
+          }
+        `}</style>
+    </div>
+  )
+}
