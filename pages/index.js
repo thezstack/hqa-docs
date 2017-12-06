ea41607ce4de5af65141423c84951b058a41fb5f
Allen Hai <hai@growinfinitely.com>
2017-12-06 17:32:34 -0600

    created section component, alternate bg color for evens / odds

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 // Helpers
 
 {
-  /*import Link from 'next/link'*/
+  /*
+import Link from 'next/link'
+import redirect from '../lib/redirect'
+export default redirect('/docs')
+*/
 }
+
 import Slider from '../components/slider'
-
-{
-  /* import redirect from '../lib/redirect'
-
-export default redirect('/docs') */
-}
+import Section from '../components/section'
 
 export default function HQA() {
   return (
     <div>
       <Slider />
+      <Section number={1} />
+      <Section number={2} />
+      <Section number={3} />
+      <Section number={4} />
       <style jsx global>{`
           * {
             margin: 0px;
