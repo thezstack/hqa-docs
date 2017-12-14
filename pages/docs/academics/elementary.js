4d5d37de57e38b7f8350b0502c6d3244911c331f
Zubair Mughal <mghlzbr@gmail.com>
2017-12-13 21:30:45 -0600

    updated intro for academics section in elementary

--- pages/docs/academics/elementary.js
+++ pages/docs/academics/elementary.js
@@ -19,11 +19,16 @@ export default withDoc({
 
 
 ## Elementary Curriculum
-In kindergarten through 2nd grade, your child establishes a firm foundation in reading, phonics, spelling, writing, mathematics, science, and social studies. In addition to these core subjects, instruction in art, music, technology integration, Spanish and French languages, and physical education all enhance your child’s learning experience.
+Houston Quran Academy is where children can explore the world around them. We stimulate their minds and nurture their hearts. We strongly believe in a balance between social and emotional learning. Our philosophy is that a child learns best when in a safe, secure, and happy environment.
+We want students to be engaged in hands-on learning prompted by their own curiosity and interests. Therefore, we base our curriculum around project-based learning. Lessons, assignments, and assessments are designed for and adjusted to each child’s level and needs.
 
-Each child is individually supported in Village’s learning community with relationships and resources that foster confidence, problem solving, creativity and a love of learning.
+In kindergarten through 2nd grade, your child establishes a firm foundation in reading, speaking, spelling, writing, mathematics, science, and social studies. In addition to these core subjects, instruction in technology, Arabic language, and physical education all enhance your child’s learning experience.
 
-In Grades 3 and 4, your child receives instruction that provides the best preparation for their continued learning and growth. This includes departmentalizing the 3rd and 4th grade classes, which are taught by specialized teachers.
+Each child is individually supported in Houston Quran Academy's learning community with relationships and resources that foster confidence, problem solving, creativity and a passion for learning.
+
+In Grades 3 to 5, your child receives instruction that provides the best preparation for their continued learning and growth.
+The curriculum at HQA covers all Texas requirements STAAR TEKS and National Standards.
+
 
 The following teaching areas are important components of our elementary school curriculum:
 
@@ -46,7 +51,7 @@ Social Studies introduces children to history, geography, and map making skills 
 The technology program develops a sense of computer literacy in each student. Students perform various computer operations, experience new technologies, and learn to use that knowledge in a responsible and independent manner. The use of technology is integrated in other subject areas of the curriculum.
 
 
-Computers and iPads in the Classroom:
+### Computers and iPads in the Classroom:
 
 Reinforce skills in other subject areas
 Practice keyboarding
@@ -55,7 +60,7 @@ Research information
 Create projects through word processing and other programs
 Explore new ideas
 
-Computer Lab:
+### Computer Lab:
 
 Increase computer literacy
 Learn proper keyboarding techniques
@@ -67,4 +72,32 @@ Explore coding and robotics
 ## Physical Education
 Activites are designed to provide daily exercise, friendly competition, and relationship building among peers. Students learn how to use their bodies more efficienty & knowledgably through a wide variety of fundametal sports. Students are tested on good sportsmanship, coordination, & motor skills.
 
+## Standarized Testing
+
+3rd Grade: Math and Reading STAAR
+
+4th Grade: Math, Reading and Writing STAAR
+
+5th Grade: Math, Science and Reading STAAR
+
+
+## IOWA
+
+The IOWA Evaluates:
+
+Language Skills
+Vocabulary
+Reading
+Language
+Word Analysis (Grades K-3 only)
+Listening (Grades K-3 only)
+Spelling
+Mathematics
+Math Concepts
+Math Computation
+Math Problem Solving
+Science, Social Studies, and Study Skills
+Social Studies
+Science Materials
+Sources of Information
 `)
