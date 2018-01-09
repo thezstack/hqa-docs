17004eb971b85b44066122f960333b7569e3c2de
Toma Tomoe <denysiuk.tina@gmail.com>
2018-01-09 22:51:24 +0200

    navbar style

--- components/navbar.js
+++ components/navbar.js
@@ -3,39 +3,58 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/">
-            <strong>HQA</strong>
-          </Link>
-        </li>
-        <li>
-          <Link href="/docs/islamic-studies/quran/memorization">
-            Islamic Studies
-          </Link>
-        </li>
-        <li>
-          <Link href="/docs/academics/montessori">Academics</Link>
-        </li>
-        <li>
-          <Link href="/docs/contact-us">Parents</Link>
-        </li>
-        <li>
-          <Link href="/docs/contact-us">Contact</Link>
-        </li>
-      </ul>
+      <div className="navbar-top">
+        <i className="menu-icon" />
+      </div>
+      <div className="dropdown-nav">
+        <ul>
+          <li>
+            <Link href="/">
+              <strong>HQA</strong>
+            </Link>
+          </li>
+          <li>
+            <Link href="/docs/islamic-studies/quran/memorization">
+              <span>Islamic Studies</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/docs/academics/montessori">
+              <span>Academics</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/docs/contact-us"><span>Parents</span></Link>
+          </li>
+          <li>
+            <Link href="/docs/contact-us"><span>Contact</span></Link>
+          </li>
+        </ul>
+      </div>
       <style jsx>{`
-        div {
-          padding: 50px;
-        }
-        ul {
-          float: left;
-        }
-        li {
-          display: inline;
-          margin: 0px 15px;
-        }
-      `}</style>
+          div {
+            padding: 0px;
+          }
+          .navbar-top {
+            height: 60px;
+            background-color: #11194F;
+            border-bottom: 1px solid #E3D961;
+          }
+          ul {
+            background-color: #11194F;
+            border-bottom: 1px solid #E3D961;
+          }
+          li {
+            display: block;
+            margin: 0px;
+            padding: 10px 20px;
+            color: #E3D961;
+            text-align: right;
+          }
+          Link {
+            color: #E3D961;
+          }
+        `}</style>
     </div>
   )
 }
